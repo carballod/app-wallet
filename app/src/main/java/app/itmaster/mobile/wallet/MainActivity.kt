c8eedb7a5f1ce835f495cd3d80824d58cf43c7ac
Daniela <danscarballo@gmail.com>
2023-11-17 21:51:38 -0300

    Login Screen

--- app/src/main/java/app/itmaster/mobile/wallet/MainActivity.kt
+++ app/src/main/java/app/itmaster/mobile/wallet/MainActivity.kt
@@ -1,11 +1,27 @@
 package app.itmaster.mobile.wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etUsername: EditText = findViewById(R.id.etUsername)
+        val etPassword: EditText = findViewById(R.id.etPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
     }
 }
