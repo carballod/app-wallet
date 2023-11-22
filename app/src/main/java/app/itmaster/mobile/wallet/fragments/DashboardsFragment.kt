22c57c766bf5f85f3233ce78509b7271233b7adc
Daniela <danscarballo@gmail.com>
2023-11-22 01:45:54 -0300

    Implementa swipe views

---
+++ app/src/main/java/app/itmaster/mobile/wallet/fragments/DashboardsFragment.kt
@@ -0,0 +1,19 @@
+package app.itmaster.mobile.wallet.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.itmaster.mobile.wallet.R
+
+class DashboardsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboards, container, false)
+    }
+}
